c8835ecec158b123fc52a1341d62e4dd8418c2f9
kexcoder <33663607+kexcode@users.noreply.github.com>
2024-07-30 16:06:44 +0200

    Delete easyspin directory

--- easyspin/Contents.m
+++
@@ -1,5 +0,0 @@
-% EasySpin - a toolbox for simulating Electron Paramagnetic Resonance spectra
-%
-%    Visit easyspin.org for the lastest version
-%    and to access the full documentation.
-%
--- easyspin/runprivate.m
+++
@@ -1,9 +0,0 @@
-function varargout = runprivate(fname,varargin)
-
-result = cell(1,nargout);
-
-[result{:}] = feval(fname,varargin{:});
-
-varargout = result;
-
-return
